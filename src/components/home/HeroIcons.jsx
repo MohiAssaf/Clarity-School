2ba08b0729354595c7dbd909ad6f03b331d52b31
MohiAssaf <mohamedabuassaf@gmail.com>
2025-08-30 23:12:39 +0300

    added new animated icons, ensuring consistent motion effects, styling, and color scheme for a cohesive visual experience.

--- src/components/home/HeroIcons.jsx
+++ src/components/home/HeroIcons.jsx
@@ -272,6 +272,443 @@ const HeroIcons = () => {
           />
         </svg>
       </motion.div>
+
+      {/* Book Icon */}
+      <motion.div
+        className="absolute bottom-1/4 left-1/4 -translate-x-1/2"
+        initial={{ opacity: 0.4, scale: 0.8, rotate: -6 }}
+        animate={{
+          opacity: [0.4, 0.8, 0.4],
+          scale: [1, 1.1, 1],
+          y: [0, -10, 0],
+          x: [0, 5, 0],
+          rotate: [-6, -12, -6],
+        }}
+        transition={{
+          duration: 12,
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="90"
+          height="90"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#155dfc"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-book-icon lucide-book"
+        >
+          <motion.path
+            d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+        </svg>
+      </motion.div>
+
+      {/* Calculator Icon */}
+      <motion.div
+        className="absolute bottom-10 right-1/6"
+        initial={{ opacity: 0.4, scale: 0.8, rotate: 10 }}
+        animate={{
+          opacity: [0.4, 0.8, 0.4],
+          scale: [1, 1.1, 1],
+          y: [0, 20, 0],
+          x: [0, -10, 0],
+          rotate: [10, 15, 10],
+        }}
+        transition={{
+          duration: 11,
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="80"
+          height="80"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#155dfc"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-calculator-icon lucide-calculator"
+        >
+          <motion.rect
+            width="16"
+            height="20"
+            x="4"
+            y="2"
+            rx="2"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.line
+            x1="8"
+            x2="16"
+            y1="6"
+            y2="6"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.line
+            x1="16"
+            x2="16"
+            y1="14"
+            y2="18"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M16 10h.01"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.1, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M12 10h.01"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.1, ease: "easeInOut", delay: 0.1 }}
+          />
+          <motion.path
+            d="M8 10h.01"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.1, ease: "easeInOut", delay: 0.2 }}
+          />
+          <motion.path
+            d="M12 14h.01"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.1, ease: "easeInOut", delay: 0.3 }}
+          />
+          <motion.path
+            d="M8 14h.01"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.1, ease: "easeInOut", delay: 0.4 }}
+          />
+          <motion.path
+            d="M12 18h.01"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.1, ease: "easeInOut", delay: 0.5 }}
+          />
+          <motion.path
+            d="M8 18h.01"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.1, ease: "easeInOut", delay: 0.6 }}
+          />
+        </svg>
+      </motion.div>
+
+      {/* Graduation Cap Icon */}
+      <motion.div
+        className="absolute top-[55%] left-[10%]"
+        initial={{ opacity: 0.4, scale: 0.8, rotate: -20 }}
+        animate={{
+          opacity: [0.4, 0.8, 0.4],
+          scale: [1, 1.1, 1],
+          y: [0, 15, 0],
+          x: [0, -5, 0],
+          rotate: [-20, -25, -20],
+        }}
+        transition={{
+          duration: 14,
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="110"
+          height="110"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#155dfc"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-graduation-cap-icon lucide-graduation-cap"
+        >
+          <motion.path
+            d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M22 10v6"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 1 }}
+          />
+          <motion.path
+            d="M6 12.5V16a6 3 0 0 0 12 0v-3.5"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 1.5 }}
+          />
+        </svg>
+      </motion.div>
+
+      {/* Globe Icon */}
+      <motion.div
+        className="absolute top-[20%] right-[30%]"
+        initial={{ opacity: 0.4, scale: 0.8, rotate: 0 }}
+        animate={{
+          opacity: [0.4, 0.8, 0.4],
+          scale: [1, 1.1, 1],
+          y: [0, 10, 0],
+          x: [0, 5, 0],
+          rotate: [0, 5, 0],
+        }}
+        transition={{
+          duration: 16,
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="90"
+          height="90"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#155dfc"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-earth-icon lucide-earth"
+        >
+          <motion.path
+            d="M21.54 15H17a2 2 0 0 0-2 2v4.54"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M7 3.34V5a3 3 0 0 0 3 3a2 2 0 0 1 2 2c0 1.1.9 2 2 2a2 2 0 0 0 2-2c0-1.1.9-2 2-2h3.17"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 1.5, ease: "easeInOut", delay: 0.5 }}
+          />
+          <motion.path
+            d="M11 21.95V18a2 2 0 0 0-2-2a2 2 0 0 1-2-2v-1a2 2 0 0 0-2-2H2.05"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 1.5, ease: "easeInOut", delay: 1 }}
+          />
+          <motion.circle
+            cx="12"
+            cy="12"
+            r="10"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+        </svg>
+      </motion.div>
+
+      {/* Book Open Icon */}
+      <motion.div
+        className="absolute top-[65%] right-[25%]"
+        initial={{ opacity: 0.4, scale: 0.8, rotate: 15 }}
+        animate={{
+          opacity: [0.4, 0.8, 0.4],
+          scale: [1, 1.1, 1],
+          y: [0, -10, 0],
+          x: [0, 10, 0],
+          rotate: [15, 20, 15],
+        }}
+        transition={{
+          duration: 13,
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="90"
+          height="90"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#155dfc"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-book-open-icon lucide-book-open"
+        >
+          <motion.path
+            d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0.5 }}
+          />
+        </svg>
+      </motion.div>
+
+      {/* Microscope Icon */}
+      <motion.div
+        className="absolute bottom-[2%] left-[45%]"
+        initial={{ opacity: 0.4, scale: 0.8, rotate: -8 }}
+        animate={{
+          opacity: [0.4, 0.8, 0.4],
+          scale: [1, 1.1, 1],
+          y: [0, -15, 0],
+          x: [0, 5, 0],
+          rotate: [-8, -13, -8],
+        }}
+        transition={{
+          duration: 12,
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="80"
+          height="80"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#155dfc"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-microscope-icon lucide-microscope"
+        >
+          <motion.path
+            d="M6 18h8"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M3 22h18"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.2 }}
+          />
+          <motion.path
+            d="M14 22a7 7 0 1 0 0-14h-1"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M9 14h2"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
+          />
+          <motion.path
+            d="M9 12a2 2 0 0 1-2-2V6h6v4a2 2 0 0 1-2 2Z"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 1.5, ease: "easeInOut", delay: 0.7 }}
+          />
+          <motion.path
+            d="M12 6V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 1, ease: "easeInOut", delay: 1 }}
+          />
+        </svg>
+      </motion.div>
+
+      {/* School Icon */}
+      <motion.div
+        className="absolute top-[5%] left-[45%]"
+        initial={{ opacity: 0.4, scale: 0.8, rotate: 10 }}
+        animate={{
+          opacity: [0.4, 0.8, 0.4],
+          scale: [1, 1.1, 1],
+          y: [0, -10, 0],
+          x: [0, 5, 0],
+          rotate: [10, 15, 10],
+        }}
+        transition={{
+          duration: 15,
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100"
+          height="100"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#155dfc"
+          strokeWidth="1"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-school-icon lucide-school"
+        >
+          <motion.path
+            d="M14 21v-3a2 2 0 0 0-4 0v3"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M18 5v16"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="m4 6 7.106-3.79a2 2 0 0 1 1.788 0L20 6"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="m6 11-3.52 2.147a1 1 0 0 0-.48.854V19a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-5a1 1 0 0 0-.48-.853L18 11"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.path
+            d="M6 5v16"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0 }}
+          />
+          <motion.circle
+            cx="12"
+            cy="9"
+            r="2"
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0 }}
+          />
+        </svg>
+      </motion.div>
     </div>
   );
 };
