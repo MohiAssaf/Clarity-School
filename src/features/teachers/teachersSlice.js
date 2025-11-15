24d42aaa2c513f2390100f84f1a447cab9d330a7
MohiAssaf <mohamedabuassaf@gmail.com>
2025-11-15 16:19:24 +0200

    fixing one of the subject name to in the dummy teachers to match the one in the dummy subjects

--- src/features/teachers/teachersSlice.js
+++ src/features/teachers/teachersSlice.js
@@ -9,7 +9,7 @@ const DUMMY_TEACHERS = [
       "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     availability: "flexible",
     assignments: [
-      { grade: 10, subject: "Math", frequency: 5 },
+      { grade: 10, subject: "Mathematics", frequency: 5 },
       { grade: 10, subject: "Physics", frequency: 3 },
     ],
   },
